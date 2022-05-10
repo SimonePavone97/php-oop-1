77da90b5484a6c955e0ced089a41abecd0713f26
Simone Pavone <simonebest56@gmail.com>
2022-05-10 16:03:26 +0200

    Quasi completo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!--Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+- è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà-->
+
+<?php
+
+class Movie {
+
+    public $nome;
+    public $anno;
+    public $genere;
+
+    public function __construct($_nome, $_anno, $_genere){
+        $this -> nome = $_nome;
+        $this -> anno = $_anno;
+        $this -> genere = $_genere;
+    };
+}
+
+
+
+$spider = new Movie ("SpiderMan 1", 2003, "Azione");
+
+echo "<p>" .  $spider -> name . "</p>";
+?>
